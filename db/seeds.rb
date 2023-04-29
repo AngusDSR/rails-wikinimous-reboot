61e02f18d4d2cbd0ff7723f590ea9b18aaacbd92
Angus Spawton-Rice <adspawtonrice@gmail.com>
2023-04-29 14:24:38 +0100

    workings and crudilicious

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,46 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# DON'T FORGET TO REQUIRE!
+require 'faker'
+
+# SET THE NUMBER TO GENERATE - WE RE-USE THIS - DRY
+ARTICLE_COUNT = 15
+
+# Single method to generate:
+def generate_article_content
+
+  # heredoc: https://blog.saeloun.com/2020/04/08/heredoc-in-ruby-and-rails#:~:text=What%20is%20heredoc%3F,method%20dynamic%20definitions%20and%20more.
+  <<-ARTICLE
+    #{Faker::Lorem.sentences(number: 4).join(' ')}
+
+    ## #{Faker::Company.buzzword} #{Faker::Company.industry}
+
+    #{Faker::Lorem.paragraphs(number: 3).join(' ')}
+
+    #{Faker::Lorem.paragraphs(number: 3).join(' ')}
+
+    **#{Faker::Lorem.word}** #{Faker::Lorem.paragraphs(number: 3).join(' ')}
+
+    ## #{Faker::Company.catch_phrase}
+
+    #{Faker::Lorem.paragraphs(number: 3).join(' ')} [link]
+
+    #{Faker::Lorem.paragraphs(number: 3).join(' ')}
+
+    #{Faker::Lorem.paragraphs(number: 3).join(' ')}
+
+    #{Faker::Lorem.paragraphs(number: 3).join(' ')}
+  ARTICLE
+end
+
+puts 'Adding articles...'
+
+ARTICLE_COUNT.times do
+  article = Article.new(
+    title: Faker::Company.catch_phrase,
+    content: generate_article_content
+  )
+  article.save
+  puts "#{article.title} added"
+end
+
+puts "#{ARTICLE_COUNT} articles added"
+puts "\"#{Article.first.title}\" ... \"#{Article.last.title}\""
